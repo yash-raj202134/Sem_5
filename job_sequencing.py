a7976c0f50a3ea844c3d1b7985ac95fee9748cfd
Yash Raj <89970907+yash-raj202134@users.noreply.github.com>
2022-10-18 23:53:32 +0530

    Update job_sequencing.py

--- job_sequencing.py
+++ job_sequencing.py
@@ -1,28 +1,24 @@
 # Lab 5 : Job sequencing Problem
 def jobsequencing(arr: list, m: int) -> list:
     '''a function to implement job sequencing problem'''
-    '''sorting the passed array 
-    on the basis of their deadlines''' 
     
+    '''sorting the passed array 
+    on the basis of their deadlines'''  
     arr = sorted(arr, key=lambda i: i[2], reverse=True) 
    
-    max_profit = 0
-    max_list = [] # an empty list to collect results
-    for a in range(len(arr)-1): # logic to get maximum item for 2 same deadlines 
-        if arr[a][2] == arr[a+1][2]:
-            max_item = arr[a]
+    count = 0 # for counting jobs added to sequence
+    job_sequence=[None]*m
+    for item in arr:
+        if count >= m:
             break
-
-    for i in range(m): # to create a slot equal to the max deadline
-        max_item = (max_item[0], 0, 0)
-        for item in arr: # iterating list.
-            if item[2] == i+1 and item[1] > max_item[1]:
-                max_item = item  # assigning the max item after it satisfies the job sequencing conditions.
-
-        max_list.append(max_item)  # appending the result 
-
-    return [[i[0],i[1]] for i in max_list] # return list of jobs.
-
+        i=0
+        for i in range(item[2]-1,-1,-1):
+            if job_sequence[i] == None:
+                job_sequence[i] = (item[0],item[1])
+                count+=1
+                break
+    
+    return job_sequence # return list of jobs.
 
 # Driver code:
 jobs = input("Enter jobs : ")   #taking jobs as input (seperated by ",")
