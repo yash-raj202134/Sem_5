60e67b0ebc80b8b302b998f942c4027a7afd7d98
Yash Raj <89970907+yash-raj202134@users.noreply.github.com>
2022-10-20 17:01:49 +0530

    Add files via upload

---
+++ adjacencyMatrix.py
@@ -0,0 +1,34 @@
+# Lab 4 :
+# WAP to represent Graph using Adjaceny Matrix
+# Adjacency Matrix representation in Python
+
+class Graph:
+    def __init__(self,size) -> None:
+        self.Nodesize = size # to track number of the nodes
+        # create and initialize matrix with 0 initially
+        self.adjMatrix = [[0]*size for i in range(size)] 
+    
+    def printmatrix(self):
+        '''returns the adjacency matrix''' 
+        print(self.adjMatrix)
+    
+    def add_edge(self,edg):
+        '''a method that adds the edges to make a graph'''
+        edge = list(edge.split(','))
+        edg = [int(i) for i in edg]
+        if edg[0] == edg[1] :
+            print(f"Same vertex {edg[0]} and {edg[1]}")
+        
+        
+        self.adjMatrix[edg[0]][edg[1]] = 1  
+        self.adjMatrix[edg[1]][edg[0]] = 1 
+    
+# driver code                 
+no_of_vertices = int(input("Enter no. of vertices or nodes : "))
+
+g = Graph(no_of_vertices)                                           
+for i in range(no_of_vertices):
+    edge = input(f"Enter edge {i} : ")   
+    g.add_edge(edge)
+    
+g.printmatrix()
